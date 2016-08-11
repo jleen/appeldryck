17f0db2c801f162026830dccae43c76c0c40180e
John Leen <jleen@saturnvalley.org>
2016-08-11 09:22:16 -0700

    Parse Dryckfiles. Transform extensions.

--- appeldryck/appeldryck.py
+++ appeldryck/appeldryck.py
@@ -1,31 +1,27 @@
+from configparser import ConfigParser
 import os
+import subprocess
 import sys
 import time
-import subprocess
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-DRYCKER = [
-        ['html', 'build', ['cat']],
-        ['sass', 'build/css', ['sass']],
-        ['js', 'build', ['babel', '--presets',
-                         '/usr/local/lib/node_modules/babel-preset-es2015']]
-]
-
-
 class Dricker(FileSystemEventHandler):
 
-    def __init__(self, transform, output_dir):
+    def __init__(self, transform, output_dir, ext):
         self.transform = transform
         self.output_dir = output_dir
+        self.ext = ext
 
     def on_any_event(self, event):
         if not event.is_directory:
             if event.event_type != 'deleted':
-                dest = os.path.join(self.output_dir,
-                                    os.path.basename(event.src_path))
+                basename = os.path.basename(event.src_path)
+                if self.ext:
+                    basename = os.path.splitext(basename)[0] + '.' + self.ext
+                dest = os.path.join(self.output_dir, basename)
                 with open(dest, 'w') as out:
                     subprocess.Popen(self.transform + [event.src_path],
                                      stdout=out)
@@ -33,10 +29,20 @@ class Dricker(FileSystemEventHandler):
 
 
 def appeldryck():
-    print('Appeldricker...')
     observer = Observer()
-    for [src, dest, transform] in DRYCKER:
-        observer.schedule(Dricker(transform, dest), src)
+
+    drycker = ConfigParser()
+    drycker.read('Dryckfile')
+
+    for dryck in drycker:
+        if dryck == 'DEFAULT':
+            continue
+        dest = drycker[dryck]['to']
+        transform = drycker[dryck]['via'].split(' ')
+        ext = drycker[dryck].get('ext', None)
+        observer.schedule(Dricker(transform, dest, ext), dryck)
+
+    print('Appeldricker...')
     observer.start()
 
     try:
