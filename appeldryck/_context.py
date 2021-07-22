06574185907d748d0193f8d01d8b2095d7cfc20a
John Leen <jleen@saturnvalley.org>
2021-07-22 16:16:58 -0400

    LaTeX escapes

--- appeldryck/_context.py
+++ appeldryck/_context.py
@@ -107,4 +107,9 @@ class LaTeXContext(Context):
 
     def escape(self, text):
         # TODO
-        return text.replace('#', '\\#').replace('&', '\\&')
+        text = text.replace('#', '\\#')
+        text = text.replace('&', '\\&')
+        text = text.replace('_', '\\_')
+        text = text.replace('^', '\\^')
+        text = text.replace('%', '\\%')
+        return text
