b3d3aee26902242724f75b0e036aee93efbe70aa
John Leen <jleen@saturnvalley.org>
2023-10-29 13:47:59 -0400

    Basic lexemes

--- appeldryck/parser/lex.py
+++ appeldryck/parser/lex.py
@@ -1,21 +1,75 @@
+import re
+
 from .ply import lex
 
-tokens = ('FUNC', 'STAR', 'LBRACE', 'RBRACE', 'NEWLINE', 'TEXT')
+tokens = ('METAEOL', 'FUNC', 'STAR', 'LBRACE', 'RBRACE', 'NEWLINE', 'TEXT')
+states = (('meta', 'exclusive'),
+          ('arg', 'exclusive'),
+         )
+
+
+#
+# Metadata definitions.
+
+# ◊metakey: metadata value<EOL>
+#
+
+def t_METATAG(t):
+    r'(^|(?<=\n))◊\w*:\s*'
+    t.lexer.begin('meta')
+    t.value = re.match('◊(\w*):', t.value).group(1)
+    return t
+
+def t_meta_METAVAL(t):
+    r'[^\n]+'
+    return t
+
+def t_meta_METAEOL(t):
+    r'\n'
+    t.lexer.lineno += 1
+    t.lexer.begin('INITIAL')
+    return t
+
+
+#
+# Python invocations.
+#
+# ◊func
+# ◊func{arg}{arg}
+# ◊{eval}
+#
 
 def t_FUNC(t):
     r'◊\w*'
+    t.value = t.value[1:]
     return t
+
+def t_EVAL(t):
+    r'◊'
+    return t
+
+def t_INITIAL_arg_LBRACE(t):
+    r'{'
+    t.lexer.push_state('arg')
+    return t
+
+def t_arg_RBRACE(t):
+    r'}'
+    t.lexer.pop_state()
+    return t
+
+def t_arg_ARG(t):
+    r'[^}]+'
+    t.lexer.lineno += t.value.count('\n')
+    return t
+
+
+#
+# Static markup.
+#
 
 def t_STAR(t):
     r'\*'
-    return t
-
-def t_LBRACE(t):
-    r'{'
-    return t
-
-def t_RBRACE(t):
-    r'}'
     return t
 
 def t_NEWLINE(t):
@@ -24,10 +78,22 @@ def t_NEWLINE(t):
     return t
 
 def t_TEXT(t):
-    r'[^!\n]+'
+    r'[^◊}\n]+'
     return t
+
+
+#
+# Errors.
+#
+
+def t_arg_error(t):
+    raise Exception(f'Unable to tokenize on line {t.lexer.lineno} at: {t.value[:20].splitlines()[0]}')
+
+def t_meta_error(t):
+    raise Exception(f'Unable to tokenize on line {t.lexer.lineno} at: {t.value[:20].splitlines()[0]}')
 
 def t_error(t):
     raise Exception(f'Unable to tokenize on line {t.lexer.lineno} at: {t.value[:20].splitlines()[0]}')
+
 
 lexer = lex.lex()
