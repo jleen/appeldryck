69d2e6229300e824bd8942c23e85383498377041
John Leen <jleen@saturnvalley.org>
2025-01-29 11:50:10 -0500

    Builder handles templates

--- appeldryck/builder.py
+++ appeldryck/builder.py
@@ -6,6 +6,8 @@ import shutil
 from pathlib import Path
 
 import appeldryck
+from . import evaluator
+from . import renderer
 
 
 SRC = 'site'
@@ -29,7 +31,9 @@ def build():
             src = root / src
             if src.suffix == '.dryck' and src.name.startswith('_'):
                 print(f'importing {src} into context')
-                contextualize(src, ctx)
+                # If there are multiple extensions, e.g. .html.dryck, then this is a template.
+                raw = len(src.suffixes) > 1
+                contextualize(src, ctx, raw)
 
     for root, dirs, files in the_walk:
         destdir = Path(DEST) / root.relative_to(SRC)
@@ -40,7 +44,10 @@ def build():
             src = root / src
             if src.name.startswith('_'): continue
             if src.suffix == '.dryck':
-                dryck(src, ctx)
+                if len(src.suffixes) == 1:
+                    process(src, ctx)
+                else:
+                    preprocess(src, ctx)
             else:
                 copy(src)
 
@@ -55,19 +62,41 @@ def copy(src):
     print(f'copying {src} to {dest}')
     shutil.copy(src, dest)
 
-def dryck(src, ctx):
+def process(src, ctx):
+    # We need to process the markup first, in order to get the template name.
+    ctx.body = indented_string(renderer.markup(ctx, src))
+    dest = Path(DEST) / src.relative_to(SRC).with_suffix(Path(ctx.template).suffix)
+    # TODO: Approximately nothing about the next line is good.
+    # HAHAHA ctx.template needs to be looked up as a function
+    template_fn = getattr(ctx, ctx.template)
+    body = evaluator.apply_func(template_fn, [], ctx, raw=True, indent=0)
+    print(f'drycking {src} as {dest}')
+    with open(dest, 'w') as out:
+        out.write(body)
+
+def indented_string(text):
+    '''Wrap a string in a function that is annotated as indented.'''
+    @appeldryck.indented
+    def indented_wrapper():
+        return text
+    return indented_wrapper
+
+def preprocess(src, ctx):
     dest = Path(DEST) / src.relative_to(SRC).with_suffix('')
     print(f'drycking {src} as {dest}')
     body = appeldryck.preprocess(ctx, src)
     with open(dest, 'w') as out:
         out.write(body)
 
-def contextualize(src, ctx):
+def contextualize(src, ctx, raw):
     name = src.stem.lstrip('_')
-    setattr(ctx, name, make_template_runner(src).__get__(ctx))
+    setattr(ctx, name, make_template_runner(src, raw).__get__(ctx))
 
-def make_template_runner(src):
+def make_template_runner(src, raw):
     @appeldryck.indented
     def run_template(self):
-        return appeldryck.render(self, src)
+        if raw:
+            return appeldryck.preprocess(self, src)
+        else:
+            return appeldryck.render(self, src)
     return run_template
