c938566047797f5501f09f9c7d000f20b9cc3249
John Leen <jleen@saturnvalley.org>
2026-09-19 16:07:30 -0400

    Vendor our only unvendored dependency

--- appeldryck/builder.py
+++ appeldryck/builder.py
@@ -2,7 +2,6 @@ import importlib
 import importlib.machinery
 import importlib.util
 import inspect
-import overlay
 import shutil
 
 from pathlib import Path
@@ -106,7 +105,7 @@ def process_dir(path: Path, ctx):
                     pass
 
                 try:
-                    process_dir(item, overlay.overlay(Subcontext, ctx))
+                    process_dir(item, overlay(Subcontext, ctx))
                 except Exception as e:
                     e.add_note(f'while rendering project directory {item}')
                     raise
@@ -168,3 +167,52 @@ def curry_file_as_function(src, raw):
             return appeldryck.render(self, src)
 
     return run_template
+
+
+# Vendored from python-overlay-class 2ca2701
+def overlay(overlay_class, base_object):
+    '''An overlay is a wrapper around an object,
+    shadowing one or more methods of the object's class.
+
+    To construct an overlay around a given base object,
+    call this function with the base object along with an
+    "overlay class" that defines the methods you wish to
+    add or shadow.
+
+    All other attribute accesses (including assignments) will be
+    delegated to the wrapped object.'''
+
+    # The wrapper object needs to have two parent classes:
+    #   1. the overlay class, for access to the new methods
+    #   2. the original class of the base object, for access to the original methods
+    #      AND so that the overlaid class conforms to the base class
+    #      (otherwise, for example, super() from a method of the base object
+    #      wouldn't work when called from an overlay)
+    class Overlaid(overlay_class, base_object.__class__):
+        # We explicitly define a trivial initializer.
+        #
+        # The base object was already initialized when it was constructed,
+        # before the overlay came into the picture.  Reinitializing it would
+        # just reset its state, which is counter to the goal of an overlay.
+        #
+        # We don’t run the overlay class’s initializer either,
+        # since you really shouldn’t define an initializer for an overlay.
+        # (An overlay has no storage of its own, so what would you be initializing?)
+        def __init__(self):
+            pass
+
+        def __getattr__(self, name):
+            # Methods defined in the overlay or the base will have already been resolved
+            # by the time we get here. So, anything else should just be delegated
+            # to the wrapped object.
+            #
+            # Note that we can't just allow multiple inheritance to take care of
+            # this, because we need to delegate to the original *object*,
+            # not just the original *class*.
+            return getattr(base_object, name)
+
+        def __setattr__(self, name, value):
+            # All writes should just go through to the wrapped object.
+            setattr(base_object, name, value)
+
+    return Overlaid()
